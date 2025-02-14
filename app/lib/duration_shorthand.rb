624618e55a169ddfe866bc14dfd0c103b5b88693
Chris Kalafarski <chris@farski.com>
2025-02-13 21:46:03 -0500

    Add a duration shorthand expansion utility

---
+++ app/lib/duration_shorthand.rb
@@ -0,0 +1,24 @@
+##
+# TODO Docs
+
+class DurationShorthand
+  def self.expand(value)
+    if /^[0-9]+[YWDhm]$/.match?(value)
+      if value.include?("Y")
+        value.to_i * 86400 * 365
+      elsif value.include?("W")
+        value.to_i * 86400 * 7
+      elsif value.include?("D")
+        value.to_i * 86400
+      elsif value.include?("h")
+        value.to_i * 3600
+      elsif value.include?("m")
+        value.to_i * 60
+      end
+    elsif /^[0-9]+$/.match?(value)
+      value.to_i
+    else
+      value
+    end
+  end
+end
