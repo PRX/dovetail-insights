71ada652d4646841576daf8e77782cfd716fc8b8
Chris Kalafarski <chris@farski.com>
2025-02-24 18:43:05 -0500

    Refactor more group member labels

--- app/helpers/group_member_label_helper.rb
+++ app/helpers/group_member_label_helper.rb
@@ -1,4 +1,16 @@
 module GroupMemberLabelHelper
+  ##
+  # Take a number of seconds and returns a simplified string when the seconds
+  # is divisible evenly into years/weeks/etcs. The string includes the
+  # resulting units.
+  # E.g., 86_400 => "1 day"
+  # E.g., 172_800 => "2 days"
+  #
+  # Any value that isn't evenly divisible remains in seconds.
+  # E.g., 1000 => "1,000 seconds"
+  #
+  # TODO Localize units
+
   def human_duration(seconds)
     if seconds % (86400 * 365) == 0
       years = seconds / (86400 * 365)
@@ -14,6 +26,17 @@ module GroupMemberLabelHelper
     end
   end
 
+  ##
+  # tktk
+
+  def clean_timestamp_string(datetime)
+    # datetime is a DateTime
+    datetime.to_s.gsub("T00:00:00+00:00", "")
+  end
+
+  ##
+  # Returns a label for a single durating range within a set of defined ranges.
+
   def duration_label(composition, group, member_descriptor)
     index_of_index = group.indices.index(member_descriptor.to_i)
 
@@ -26,38 +49,77 @@ module GroupMemberLabelHelper
     end
   end
 
+  ##
+  # Formats groups resulting from extacted Timestamp dimensions into more
+  # human-readable strings
+
+  def timestamp_extract_label(composition, group, member_descriptor)
+    case group.extract
+    when :month
+      Date::ABBR_MONTHNAMES[member_descriptor.to_i]
+    when :week
+      "Week #{member_descriptor}"
+    when :day
+      member_descriptor.to_i.ordinalize
+    when :day_of_week
+      ["", "Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"][member_descriptor.to_i]
+    when :hour
+      "#{member_descriptor}:00"
+    end
+  end
+
+  ##
+  # Returns a label for a single time/date range within a set of defined ranges.
+
+  def timestamp_range_label(composition, group, member_descriptor)
+    index_of_index = group.abs_indices.index(member_descriptor)
+
+    if member_descriptor == Compositions::Components::Group::TERMINATOR_INDEX
+      "After #{clean_timestamp_string group.abs_indices.last}"
+    elsif index_of_index == 0
+      "Before #{clean_timestamp_string member_descriptor}"
+    else
+      "Between #{clean_timestamp_string group.abs_indices[index_of_index - 1]} and #{clean_timestamp_string member_descriptor}"
+    end
+  end
+
+  ##
+  # Handes labels for various types of Timestamp groups, like range, extact
+  # and truncate.
+
+  def timestamp_label(composition, group, member_descriptor)
+    if group.extract
+      timestamp_extract_label(composition, group, member_descriptor)
+    elsif group.indices
+      timestamp_range_label(composition, group, member_descriptor)
+    end
+  end
+
+  ##
+  # For a given group member, this is meant to return the label most appropriate
+  # to display to a user for overall ease-of-use.
+  #
+  # Some dimensions use their member descriptor as the user-facing label. Some
+  # define and exhibit property, which is a value from the database that is
+  # meant to be used instead of the descriptor. In some situations, the
+  # descriptor or exhibitition are further refined or replaced by this helper.
+  #
+  # Some times refinement happens for an entire class of members, like how all
+  # duration descriptors are replaced to be more meaningful and readible. Other
+  # time, an individual value may need to be replaced before being displayed to
+  # the user, like we may want to override the name of a specific user agent.
+
   def member_label(composition, group, member_descriptor)
     return "UNKNOWN" unless member_descriptor
 
     dimension_def = DataSchema.dimensions[group.dimension.to_s]
 
     if dimension_def["Type"] == "Duration"
-      return duration_label(composition, group, member_descriptor)
+      duration_label(composition, group, member_descriptor)
+    elsif dimension_def["Type"] == "Timestamp"
+      timestamp_label(composition, group, member_descriptor)
+    else
+      composition.results.group_member_exhibition(group, member_descriptor)
     end
-
-    if dimension_def["Type"] == "Timestamp"
-      if group.extract
-        case group.extract
-        when :month
-          return Date::ABBR_MONTHNAMES[member_descriptor.to_i]
-        when :week
-          return "Week #{member_descriptor}"
-        when :day
-          return member_descriptor.to_i.ordinalize
-        when :day_of_week
-          return ["", "Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"][member_descriptor.to_i]
-        when :hour
-          return "#{member_descriptor}:00"
-        end
-      elsif group.indices
-        if member_descriptor.starts_with? "LT"
-          return member_descriptor.sub("LT", "Before")
-        elsif member_descriptor.starts_with? "GTE"
-          return member_descriptor.sub("GTE", "After")
-        end
-      end
-    end
-
-    composition.results.group_member_exhibition(group, member_descriptor)
   end
 end
