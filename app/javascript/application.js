98513e5e731602339067d432b4620140b9f83b52
Chris Kalafarski <chris@farski.com>
2025-02-24 13:31:36 -0500

    Enable slim-select for filter selects

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -2,3 +2,9 @@
 import "@hotwired/turbo-rails";
 // eslint-disable-next-line import/no-unresolved
 import "controllers";
+
+document.querySelectorAll("#filters select.token").forEach((el) => {
+  new SlimSelect({
+    select: el,
+  });
+});
