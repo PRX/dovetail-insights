e0f3e1b8e3896c6b1d2561137dc70cd6943019b9
Chris Kalafarski <chris@farski.com>
2025-03-03 18:19:17 -0500

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -17,7 +17,7 @@ Rails.application.routes.draw do
   # get "manifest" => "rails/pwa#manifest", as: :pwa_manifest
   # get "service-worker" => "rails/pwa#service_worker", as: :pwa_service_worker
 
-  get "/data-explorer", to: "data_explorer#index"
+  get "/data-explorer", to: "data_explorer#index", as: :data_explorer
   get "/data-explorer/export", to: "data_explorer#export"
 
   get "/.well-known/change-password", to: redirect("https://#{ENV["ID_HOST"]}/.well-known/change-password", status: 302)
