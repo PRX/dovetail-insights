3e5f939a615a787800ef711be7df827c09313b55
Chris Kalafarski <chris@farski.com>
2025-03-04 13:55:33 -0500

    Improve default sorting

--- app/helpers/results_helper.rb
+++ app/helpers/results_helper.rb
@@ -12,20 +12,26 @@ module ResultsHelper
       if group
         dimension_def = DataSchema.dimensions[group.dimension.to_s]
 
-        if dimension_def["SortProperties"].present?
-          dimension_def["SortProperties"].map do |sort_property_name|
+        # Return an array that sort_by will use for sorting. The first element
+        # is always explicitly controlled within this method, other values
+        # should follow that.
+        if !member_descriptor
+          # Force null groups to the bottom
+          [99]
+        elsif dimension_def["SortProperties"].present?
+          [0, *dimension_def["SortProperties"].map do |sort_property_name|
             @composition.results.group_sort_descriptor(group, member_descriptor, sort_property_name)
-          end
+          end]
         elsif member_descriptor == member_descriptor.to_i.to_s
           # If this descriptor looks like an integer, treat it as an actual
           # integer to get 1,2,10 rather than 1,10,2
-          member_descriptor.to_i
+          [0, member_descriptor.to_i]
         else
           # Otherwise, sort by the final label
-          member_label(@composition, group, member_descriptor)
+          [0, member_label(@composition, group, member_descriptor)]
         end
       end
-    end.reverse
+    end
 
     # TODO Only reverse if there's a sort property, and even that is not ideal,
     # the schema should be able to indicate sort order
