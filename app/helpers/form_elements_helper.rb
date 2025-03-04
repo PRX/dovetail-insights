7033232b8f5f6a49af615715bb7a7f0da2dd2c34
Chris Kalafarski <chris@farski.com>
2025-03-03 21:32:01 -0500

    Add docs

--- app/helpers/form_elements_helper.rb
+++ app/helpers/form_elements_helper.rb
@@ -1,4 +1,8 @@
 module FormElementsHelper
+  ##
+  # Returns `selected="selected"` when the condition is true. The condition can
+  # either be a single boolean, or two values which will be compared
+
   def select_eq(bool_or_expected, actual = nil)
     if actual
       html_attributes(selected: :selected) if expected == actual
