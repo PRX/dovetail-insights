aab8ca5991d07f2dce2b3f9a3acf8f8f8f2a777f
Chris Kalafarski <chris@farski.com>
2025-03-07 15:00:33 -0500

    Fix sorting

--- app/javascript/controllers/data_explorer/results_table_sort_controller.js
+++ app/javascript/controllers/data_explorer/results_table_sort_controller.js
@@ -11,30 +11,47 @@ export default class extends Controller {
   sort(event) {
     // TODO support multiple metrics and comparisons
 
-    const dimension = event.target.dataset.sortGroupDimension;
-    const metric = event.target.dataset.sortMetric;
-    const group2MemberDescriptor = event.target.getAttribute(
-      "data-dx-group-2-member-descriptor",
-    );
+    const metric = event.target.dataset.dxMetric;
 
-    if (event.target.dataset.sortConfig?.startsWith("0,")) {
+    let selector;
+    let dimensionName;
+    let memberDescriptor;
+    if (event.target.getAttribute("data-dx-group-1-dimension-name")) {
+      selector = "data-dx-group-1-dimension-name";
+      dimensionName = event.target.getAttribute(
+        "data-dx-group-1-dimension-name",
+      );
+      memberDescriptor = event.target.getAttribute(
+        "data-dx-group-1-member-descriptor",
+      );
+    } else if (event.target.getAttribute("data-dx-group-2-dimension-name")) {
+      selector = "data-dx-group-2-dimension-name";
+      dimensionName = event.target.getAttribute(
+        "data-dx-group-2-dimension-name",
+      );
+      memberDescriptor = event.target.getAttribute(
+        "data-dx-group-2-member-descriptor",
+      );
+    }
+
+    if (event.target.dataset.dxSortConfig?.startsWith("0,")) {
       // this is already the active sort for this dimension
 
-      if (event.target.dataset.sortConfig.endsWith(",ASC")) {
-        event.target.dataset.sortConfig = "0,DESC";
+      if (event.target.dataset.dxSortConfig.endsWith(",ASC")) {
+        event.target.dataset.dxSortConfig = "0,DESC";
       } else {
-        event.target.dataset.sortConfig = "0,ASC";
+        event.target.dataset.dxSortConfig = "0,ASC";
       }
     } else {
       // Reset the sort property of all elements that belong to this group
       this.tableTarget
-        .querySelectorAll(`*[data-sort-group-dimension="${dimension}"]`)
+        .querySelectorAll(`*[${selector}="${dimensionName}"]`)
         .forEach((el) => {
-          el.dataset.sortConfig = "";
+          el.dataset.dxSortConfig = "";
         });
 
       // Set the config for the chosen element to make it the active sort
-      event.target.dataset.sortConfig = "0,DESC";
+      event.target.dataset.dxSortConfig = "0,DESC";
     }
 
     const containingRow = event.target.closest("tr");
@@ -49,10 +66,10 @@ export default class extends Controller {
       // group and metric
       const sortedDataRows = this.rowTargets.sort((tr1, tr2) => {
         const tr1Cell = tr1.querySelector(
-          `td[data-dx-metric="${metric}"][data-dx-group-2-member-descriptor="${group2MemberDescriptor}"]`,
+          `td[data-dx-metric="${metric}"][data-dx-group-2-member-descriptor="${memberDescriptor}"]`,
         );
         const tr2Cell = tr2.querySelector(
-          `td[data-dx-metric="${metric}"][data-dx-group-2-member-descriptor="${group2MemberDescriptor}"]`,
+          `td[data-dx-metric="${metric}"][data-dx-group-2-member-descriptor="${memberDescriptor}"]`,
         );
 
         if (!tr1Cell || !tr2Cell) {
@@ -64,61 +81,12 @@ export default class extends Controller {
 
       const body = this.tableTarget.querySelector("tbody");
 
-      if (event.target.dataset.sortConfig.endsWith(",ASC")) {
+      if (event.target.dataset.dxSortConfig.endsWith(",ASC")) {
         sortedDataRows.reverse();
       }
 
       sortedDataRows.forEach((el) => el.remove());
       sortedDataRows.forEach((el) => body.append(el));
     }
-
-    return;
-    // event.preventDefault();
-
-    // const button = event.target;
-
-    // const header = button.closest("td");
-
-    // const metric = header.getAttribute("data-highlight-metric");
-    // const group = header.getAttribute("data-highlight-group-2");
-
-    // // Collect all the data rows
-    // const dataRows = [
-    //   ...document.querySelectorAll("#results table tbody tr:not(.totals)"),
-    // ];
-
-    // // Sort the rows by the values found in cells that match the button's
-    // // group and metric
-    // const sortedDataRows = dataRows.sort((tr1, tr2) => {
-    //   const tr1Cell = tr1.querySelector(
-    //     `td[data-highlight-metric="${metric}"][data-highlight-group-2="${group}"]`,
-    //   );
-    //   const tr2Cell = tr2.querySelector(
-    //     `td[data-highlight-metric="${metric}"][data-highlight-group-2="${group}"]`,
-    //   );
-
-    //   if (!tr1Cell || !tr2Cell) {
-    //     return -1;
-    //   }
-
-    //   return +tr2Cell.dataset.rawValue - +tr1Cell.dataset.rawValue;
-    // });
-
-    // if (!button.dataset.direction) {
-    //   button.dataset.direction = "desc";
-    //   button.innerText = "↓";
-    // } else if (button.dataset.direction === "desc") {
-    //   button.dataset.direction = "asc";
-    //   button.innerText = "↑";
-    //   sortedDataRows.reverse();
-    // } else if (button.dataset.direction === "asc") {
-    //   button.innerText = "↓";
-    //   button.dataset.direction = "desc";
-    // }
-
-    // const body = document.querySelector("#results table tbody");
-
-    // sortedDataRows.forEach((el) => el.remove());
-    // sortedDataRows.forEach((el) => body.append(el));
   }
 }
