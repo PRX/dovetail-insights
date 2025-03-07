07a4646c61239d6f573773cd2ebe99e82ca0b933
Chris Kalafarski <chris@farski.com>
2025-03-07 14:46:03 -0500

    Set after sign in path

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,7 @@ class ApplicationController < ActionController::Base
   # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
   allow_browser versions: :modern
 
-  before_action :authenticate! # authenticate! is provided by prx_auth-rails
+  before_action :set_after_sign_in_path, :authenticate! # authenticate! is provided by prx_auth-rails
   before_action :current_user_podcast_accounts
 
   protected
