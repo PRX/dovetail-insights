86dbb6adc10066afc93d484f6ec1979c5f94bcc1
Chris Kalafarski <chris@farski.com>
2025-03-06 21:04:38 -0500

    Support BigQuery dataset selection from envar

--- config/initializers/data_schema.rb
+++ config/initializers/data_schema.rb
@@ -5,7 +5,7 @@ DataSchema = OpenStruct.new
 DataSchema.dimensions = YAML.load_file(File.join(Rails.root, "db", "data_schema", "dimensions.yml"))["Dimensions"]
 DataSchema.properties = YAML.load_file(File.join(Rails.root, "db", "data_schema", "properties.yml"))["Properties"]
 DataSchema.metrics = YAML.load_file(File.join(Rails.root, "db", "data_schema", "metrics.yml"))["Metrics"]
-DataSchema.tables = YAML.load_file(File.join(Rails.root, "db", "data_schema", "tables.yml"))["Tables"]
+DataSchema.tables = YAML.load(ERB.new(File.read(File.join(Rails.root, "db", "data_schema", "tables.yml"))).result)["Tables"]
 
 class DataSchemaUtil
   ##
