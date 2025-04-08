a661fc64638c2701394fb6fd20e825274d739163
Chris Kalafarski <chris@farski.com>
2025-04-08 11:13:08 -0400

    Update prx_auth-rails to 5.0 and related app code

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,6 @@ class ApplicationController < ActionController::Base
   # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
   allow_browser versions: :modern
 
-  before_action :set_after_sign_in_path, :authenticate! # authenticate! is provided by prx_auth-rails
   before_action :current_user_podcast_accounts
 
   protected
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,6 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  mount PrxAuth::Rails::Engine => "/auth", :as => "prx_auth_engine"
-
   namespace :admin do
     resources :composition_result_metadata_logs
 
