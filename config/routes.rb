f0527ec886d625ba707952f565cd86b605136244
Chris Kalafarski <chris@farski.com>
2025-04-24 14:27:30 -0400

    Bump prx_auth-rails from 5.0.1 to 5.1.0

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
+  prx_auth_routes
+
   namespace :admin do
     resources :composition_result_metadata_logs
 
